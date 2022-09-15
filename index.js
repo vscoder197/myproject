bfdbe4d7fca1751bd8c7b8aa3c4f36ac95bbd2e5
vishal <vishalpr197@gmail.com>
2022-09-15 14:46:33 +0530

    adding start script

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use("/", (req, res) => {
   res.send("hey there");
 });
 
-app.listen(4000);
+app.listen(port);
